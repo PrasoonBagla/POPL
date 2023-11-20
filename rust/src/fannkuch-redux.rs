1a7d86f02feca48b9e29afe7f9d6897c340faf40
Prasoon Bagla <prasoonbagla@Prasoons-MacBook-Air.local>
2023-11-20 19:22:04 +0530

    main

--- rust/src/fannkuch-redux.rs
+++ rust/src/fannkuch-redux.rs
@@ -1,13 +1,19 @@
 use std::{cmp, mem,time};
 use std::thread;
-
+use psutil::process::Process;
+ 
+struct BenchmarkResult {
+    execution_time: time::Duration,
+    peak_memory: u64, // Add a field for peak memory usage
+}
+ 
 fn rotate(x: &mut [i32]) {
     let mut prev = x[0];
     for place in x.iter_mut().rev() {
         prev = mem::replace(place, prev)
     }
 }
-
+ 
 fn next_permutation(perm: &mut [i32], count: &mut [i32]) {
     for i in 1..perm.len() {
         rotate(&mut perm[.. i + 1]);
@@ -20,12 +26,12 @@ fn next_permutation(perm: &mut [i32], count: &mut [i32]) {
         }
     }
 }
-
+ 
 #[derive(Clone, Copy)]
 struct P {
     p: [i32; 16],
 }
-
+ 
 #[derive(Clone, Copy)]
 struct Perm {
     cnt: [i32; 16],
@@ -34,7 +40,7 @@ struct Perm {
     permcount: u32,
     perm: P,
 }
-
+ 
 impl Perm {
     fn new(n: u32) -> Perm {
         let mut fact = [1; 16];
@@ -49,14 +55,14 @@ impl Perm {
             perm: P { p: [0; 16 ] }
         }
     }
-
+ 
     fn get(&mut self, mut idx: i32) -> P {
         let mut pp = [0u8; 16];
         self.permcount = idx as u32;
         for (i, place) in self.perm.p.iter_mut().enumerate() {
             *place = i as i32 + 1;
         }
-
+ 
         for i in (1 .. self.n as usize).rev() {
             let d = idx / self.fact[i] as i32;
             self.cnt[i] = d;
@@ -64,71 +70,71 @@ impl Perm {
             for (place, val) in pp.iter_mut().zip(self.perm.p[..(i+1)].iter()) {
                 *place = (*val) as u8
             }
-
+ 
             let d = d as usize;
             for j in 0 .. i + 1 {
                 self.perm.p[j] = if j + d <= i {pp[j + d]} else {pp[j+d-i-1]} as i32;
             }
         }
-
+ 
         self.perm
     }
-
+ 
     fn count(&self) -> u32 { self.permcount }
     fn max(&self) -> u32 { self.fact[self.n as usize] }
-
+ 
     fn next(&mut self) -> P {
         next_permutation(&mut self.perm.p, &mut self.cnt);
         self.permcount += 1;
-
+ 
         self.perm
     }
 }
-
-
+ 
+ 
 fn reverse(tperm: &mut [i32], k: usize) {
     tperm[..k].reverse()
 }
-
+ 
 fn work(mut perm: Perm, n: usize, max: usize) -> (i32, i32) {
     let mut checksum = 0;
     let mut maxflips = 0;
-
+ 
     let mut p = perm.get(n as i32);
-
+ 
     while perm.count() < max as u32 {
         let mut flips = 0;
-
+ 
         while p.p[0] != 1 {
             let k = p.p[0] as usize;
             reverse(&mut p.p, k);
             flips += 1;
         }
-
+ 
         checksum += if perm.count() % 2 == 0 {flips} else {-flips};
         maxflips = cmp::max(maxflips, flips);
-
+ 
         p = perm.next();
     }
-
+ 
     (checksum, maxflips)
 }
-
+ 
 fn fannkuch(n: i32) -> (i32, i32) {
     let perm = Perm::new(n as u32);
-
+ 
     let n = 4;
     let mut futures = vec![];
     let k = perm.max() / n;
-
+ 
     for j in (0..).map(|x| x * k).take_while(|&j| j < k * n) {
         let max = cmp::min(j+k, perm.max());
-
+ 
         futures.push(thread::spawn(move|| {
             work(perm, j as usize, max as usize)
         }))
     }
-
+ 
     let mut checksum = 0;
     let mut maxflips = 0;
     for fut in futures.into_iter() {
@@ -140,20 +146,20 @@ fn fannkuch(n: i32) -> (i32, i32) {
 }
 fn fannkuch_benchmarked(n: i32) -> (i32, i32, BenchmarkResult) {
     let start = time::Instant::now(); // Start timing
-
+ 
     let perm = Perm::new(n as u32);
     let n = 4;
     let mut futures = vec![];
     let k = perm.max() / n;
-
+ 
     for j in (0..).map(|x| x * k).take_while(|&j| j < k * n) {
         let max = cmp::min(j+k, perm.max());
-
+ 
         futures.push(thread::spawn(move|| {
             work(perm, j as usize, max as usize)
         }))
     }
-
+ 
     let mut checksum = 0;
     let mut maxflips = 0;
     for fut in futures.into_iter() {
@@ -161,27 +167,28 @@ fn fannkuch_benchmarked(n: i32) -> (i32, i32, BenchmarkResult) {
         checksum += cs;
         maxflips = cmp::max(maxflips, mf);
     }
-
+ 
     let duration = start.elapsed(); // End timing
-
-    (checksum, maxflips, BenchmarkResult { execution_time: duration })
-}
-
+     // Get the current process
+     let process = Process::current().unwrap();
+     // Get the peak memory usage
+     let peak_memory = process.memory_info().unwrap().rss();
+ 
+     (checksum, maxflips, BenchmarkResult { execution_time: duration, peak_memory: peak_memory })
+    }
+ 
 fn main() {
     let n = std::env::args_os().nth(1)
         .and_then(|s| s.into_string().ok())
         .and_then(|n| n.parse().ok())
         .unwrap_or(7);
-
+ 
     let (checksum, maxflips) = fannkuch(n);
     println!("{}\nPfannkuchen({}) = {}", checksum, n, maxflips);
     let (checksum, maxflips, benchmark) = fannkuch_benchmarked(n);
     // println!("{}\nPfannkuchen({}) = {}", checksum, n, maxflips);
     println!("Time elapsed: {:?}", benchmark.execution_time); // Print elapsed time
+    println!("Peak memory usage: {} bytes", benchmark.peak_memory); // Print peak memory usage
+ 
 }
-struct BenchmarkResult {
-    execution_time: time::Duration,
-    // Add additional fields here for other metrics like memory usage, etc.
-}
-
-
+ 
